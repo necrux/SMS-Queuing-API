1d63cf1bcbe03f5587af7251eb9d2d923edeb335
wes <whendersonii@gmail.com>
2017-10-06 20:53:53 -0500

    Removed the anonymous function.

--- sms.go
+++ sms.go
@@ -10,21 +10,23 @@ const (
   port = ":5555"
 )
 
+func parse_body(w http.ResponseWriter, r *http.Request) {
+  r.ParseForm()
+
+  fmt.Printf("NUMBER => %s\n", r.FormValue("number"))
+  fmt.Printf("CARRIER => %s\n", r.FormValue("carrier"))
+  fmt.Printf("HOUR => %s\n", r.FormValue("hour"))
+  fmt.Printf("MINUTE => %s\n", r.FormValue("minute"))
+  fmt.Printf("MESSAGE => %s\n", r.FormValue("message"))
+
+  fmt.Fprintln(w, "Message queued.")
+  }
+
 func main() {
   mux := http.NewServeMux()
   fmt.Printf("Started server at http://localhost%v.\n", port)
 
-  mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    r.ParseForm()
-
-    fmt.Printf("NUMBER => %s\n", r.FormValue("number"))
-    fmt.Printf("CARRIER => %s\n", r.FormValue("carrier"))
-    fmt.Printf("HOUR => %s\n", r.FormValue("hour"))
-    fmt.Printf("MINUTE => %s\n", r.FormValue("minute"))
-    fmt.Printf("MESSAGE => %s\n", r.FormValue("message"))
-
-    fmt.Fprintln(w, "Message queued.")
-  })
+  mux.HandleFunc("/", parse_body)
 
   http.ListenAndServe(port, mux)
 }
