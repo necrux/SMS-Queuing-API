185094d3c82c8f84f40c6d9345f67b2cac0a1f18
wes <whendersonii@gmail.com>
2017-10-12 21:46:36 -0500

    Working on the JSON parsing portion.

--- sms.go
+++ sms.go
@@ -1,10 +1,18 @@
 package main
 
 import (
-  //"encoding/json"
+  "encoding/json"
   "fmt"
   "net/http"
 )
+
+type Request struct {
+  Number  string `json:"number"`
+  Carrier string `json:"carrier"`
+  Hour    string `json:"hour"`
+  Minute  string `json:"minute"`
+  Message string `json:"message"`
+}
 
 const (
   socket = "127.0.0.1:5555"
@@ -22,6 +30,24 @@ func parseBody(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintln(w, "Message queued.")
   }
 
+func parseJSONBody(w http.ResponseWriter, r *http.Request) {
+  var request Request
+  if r.Body == nil {
+    http.Error(w, "Please send a request body", 400)
+    return
+  }
+
+  err := json.NewDecoder(r.Body).Decode(&request)
+
+  if err != nil {
+    http.Error(w, err.Error(), 400)
+    return
+  }
+
+  fmt.Fprintf(w, request.Number)
+  fmt.Fprintln(w, "Message queued.")
+  }
+
 func main() {
   mux := http.NewServeMux()
   fmt.Printf("Started server at %v.\n", socket)
@@ -29,4 +55,5 @@ func main() {
   mux.HandleFunc("/send", parseBody)
 
   http.ListenAndServe(socket, mux)
+  mux.HandleFunc("/json", parseJSONBody)
 }
